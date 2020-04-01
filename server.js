1c92973101e4b8f60b3edf30314c1e4330db75d3
Pajamajoker <46530662+Pajamajoker@users.noreply.github.com>
2020-04-01 11:32:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ io.on('connection', socket => {
     })
 });
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+server.listen(process.env.PORT ||8000, () => console.log('server is running on port 8000'));
 
 
